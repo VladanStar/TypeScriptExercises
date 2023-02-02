f90676beff1fea1ce2929fc5ac6fd398136a587e
VladanStar <vladancupric@gmail.com>
2023-02-02 12:36:11 +0100

    Add

---
+++ test03.js
@@ -0,0 +1,13 @@
+"use strict";
+function average(a, b, c) {
+    let count = 1;
+    let suma = a;
+    count++;
+    suma += b;
+    if (typeof c != undefined) {
+        count++;
+        suma += b;
+    }
+    return "Average is " + suma / count;
+}
+console.log(average(4, 6));
---
+++ test03.ts
@@ -0,0 +1,12 @@
+function average (a:number, b:number, c?:number):string {
+    let count= 1;
+    let suma = a;
+    count++;
+    suma +=b;
+    if(typeof c !=undefined ){
+        count++;
+        suma+=b;
+    }
+    return "Average is " + suma/count
+}
+console.log(average(4,6))
