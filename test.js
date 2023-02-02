885e823c83fe503f0e93e80147210c3e49b72115
VladanStar <vladancupric@gmail.com>
2023-02-02 09:54:23 +0100

    Add

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
 "use strict";
-let vladan = "Vladan Cupric";
-console.log(vladan);
+// let vladan:string = "Vladan Cupric";
+// console.log(vladan);
+var TipPrevoza;
+(function (TipPrevoza) {
+    TipPrevoza[TipPrevoza["Automobil"] = 0] = "Automobil";
+    TipPrevoza[TipPrevoza["Autobus"] = 1] = "Autobus";
+    TipPrevoza[TipPrevoza["Tramvaj"] = 2] = "Tramvaj";
+    TipPrevoza[TipPrevoza["Ostalo"] = 3] = "Ostalo";
+})(TipPrevoza || (TipPrevoza = {}));
+let mojTip = TipPrevoza.Automobil;
+let imeMogTipa = TipPrevoza[mojTip];
+console.log(imeMogTipa);
--- test.ts
+++ test.ts
@@ -1,2 +1,12 @@
-let vladan:string = "Vladan Cupric";
-console.log(vladan)
+// let vladan:string = "Vladan Cupric";
+// console.log(vladan);
+
+enum TipPrevoza {
+    Automobil,
+    Autobus,
+    Tramvaj,
+    Ostalo
+}
+let mojTip = TipPrevoza.Automobil;
+let imeMogTipa = TipPrevoza[mojTip];
+console.log(imeMogTipa)
