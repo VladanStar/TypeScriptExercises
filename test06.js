07347964d2800fbb4030e852abec38ab92f2374b
VladanStar <vladancupric@gmail.com>
2023-02-03 12:57:13 +0100

    Ad

---
+++ test06.js
@@ -0,0 +1,7 @@
+"use strict";
+function saberi(a, b) {
+    let suma;
+    suma = a + b;
+    return suma;
+}
+console.log(saberi(2, 5));
---
+++ test06.ts
@@ -0,0 +1,6 @@
+function saberi(a:number, b:number):number{
+    let suma:number;
+    suma = a+b;
+    return suma;
+}
+console.log(saberi(2,5))
